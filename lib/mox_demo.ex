7992e610c1fccce738b486592e4aacf3d3d59879
Mat Trudel <mtrudel@pagerduty.com>
2023-07-14 16:36:24 -0400

    Implement tests 2-4

--- lib/mox_demo.ex
+++ lib/mox_demo.ex
@@ -10,4 +10,11 @@ defmodule MoxDemo do
     {:ok, humidity} = MoxDemo.WeatherAPI.humidity({lat, long})
     "Current humidity is #{humidity}%"
   end
+
+  def display_temp_in_toronto_and_san_francisco do
+    {:ok, toronto_temp} = MoxDemo.WeatherAPI.temp({43.6532, 79.3832})
+    {:ok, san_francisco_temp} = MoxDemo.WeatherAPI.temp({37.7749, 122.4194})
+
+    "Current temperature in Toronto is #{toronto_temp}°; while it is #{san_francisco_temp}° in San Francisco"
+  end
 end
--- test/mox_demo_test.exs
+++ test/mox_demo_test.exs
@@ -52,4 +52,105 @@ defmodule MoxDemoTest do
     # Your job is to make the following assertion pass
     assert MoxDemo.display_humidity({0, 0}) == "Current humidity is 100%"
   end
+
+  #
+  # TEST 2: Making your mocks callable multiple times
+  #
+  # This test shows you how to make your mocks callable multiple times.
+  # 
+  # By default, mocks are *validating*, which means that they validate that they get called during
+  # your test. Mox will ensure that each mock is called the expected number of times (1 by
+  # default) witnin each test, and will fail the test if this is not the case (In the next test
+  # we'll see a variation of mocks called 'stubs' which solve this problem in a different way).
+  #
+  # This test is currently failing since our `display_temp_in_toronto_and_san_francisco/0`
+  # function calls the Weather API twice; once for each city.
+  #
+  # To make this test pass, we'll want to update our `temp/1` mock to be callable multiple times
+  # (twice in this example). Don't worry about returning sensible data for each location; we'll
+  # get to that in a later test.
+  #
+  # HINTS:
+  #
+  # 1. Take a look at the documentation for [Mox.expect/4](https://hexdocs.pm/mox/Mox.html#expect/4)
+  #    to see how to get Mox to allow a mock to be called multiple times.
+  #
+
+  # IMPORTANT! Remove / comment out the following `@tag :skip` line to start running this test
+  @tag :skip
+  test "TEST 2: Making your mocks callable multiple times" do
+    expect(MoxDemo.MockWeatherAPI, :temp, fn _ ->
+      {:ok, -34}
+    end)
+
+    assert MoxDemo.display_temp_in_toronto_and_san_francisco() ==
+             "Current temperature in Toronto is -34°; while it is -34° in San Francisco"
+  end
+
+  #
+  # TEST 3: Making your mocks callable multiple times using stubs
+  #
+  # This test shows you how to make your mocks callable multiple times by using stubs instead of
+  # mocks
+  # 
+  # As we saw in the previous test, mocks are *validating*, which means that they will ensure that
+  # they are called the expected number of times (1 by default) and will fail the test if this is
+  # not the case. This is useful behaviour in most cases, but sometimes you want to just let your
+  # code call a mocked function as often as it wants to. This is where stubs come in.
+  #
+  # Stubs are basically just mocks that do not validate. A stub acts exactly like a mock does,
+  # expect that it can be called any number of times (including 0!) without error.
+  #
+  # Mox supports declaring stubs using the [Mox.stub/3](https://hexdocs.pm/mox/Mox.html#stub/3) function.
+  #
+  # This test is currently failing since our `display_temp_in_toronto_and_san_francisco/0`
+  # function calls the Weather API twice; once for each city.
+  #
+  # To make this test pass, we'll want to implement a stub for `temp/1`, using the `Mox.stub/3`
+  # function to register the stub. Don't worry about returning sensible data for each location;
+  # we'll get to that in a later test.
+  #
+  # HINTS:
+  #
+  # 1. `Mox.stub/3` behaves the exact same way as `Mox.expect/4` does, just without validating the
+  #    number of times it gets called
+  #
+
+  # IMPORTANT! Remove / comment out the following `@tag :skip` line to start running this test
+  @tag :skip
+  test "TEST 3: Making your mocks callable multiple times using stubs" do
+    assert MoxDemo.display_temp_in_toronto_and_san_francisco() ==
+             "Current temperature in Toronto is -34°; while it is -34° in San Francisco"
+  end
+
+  #
+  # TEST 4: Making your mocks handle input parameters
+  #
+  # This test shows you how to mock functions that make use of their input parameters.
+  #
+  # This test is currently failing since our mocked `temp/1` function always returns the same
+  # temperature value. As a result we're seeing the same temperature in both Toronto and San
+  # Francisco.
+  #
+  # To make this test pass, we'll want to improve our `temp/1` mock to return hotter temperatures 
+  # closer to the equator. This will demonstrate that the `MoxDemo.display_temperature/1` function
+  # is actually passing along the lat/long parameters to the Weather API.
+  #
+  # HINTS:
+  #
+  # 1. We're taking a simplistic view of weather; any location with a latitude between -40 and +40
+  #    degrees of latitude should have a temperature of 20°, and any point north / south of that
+  #    range should have a temperature of 15°.
+  #
+
+  # IMPORTANT! Remove / comment out the following `@tag :skip` line to start running this test
+  @tag :skip
+  test "TEST 4: Making your mocks handle input parameters" do
+    expect(MoxDemo.MockWeatherAPI, :temp, 2, fn {_lat, _long} ->
+      {:ok, 20}
+    end)
+
+    assert MoxDemo.display_temp_in_toronto_and_san_francisco() ==
+             "Current temperature in Toronto is 15°; while it is 20° in San Francisco"
+  end
 end
