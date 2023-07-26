35f7e529a9d805a0212691b35295da3c72b1a875
Mat Trudel <mtrudel@pagerduty.com>
2023-07-26 11:42:44 -0400

    Lower mix version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MoxDemo.MixProject do
     [
       app: :mox_demo,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
